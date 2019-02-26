417ad7104b337b05520654fdeaf1d8efdabbeb01
Kiran Wotherspoon <kirawoth947@ed.amdsb.ca>
2019-02-26 09:06:19 -0500

    Add project files.

---
+++ ScreenSaver/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ScreenSaver
+{
+    class Ball
+    {
+        public int x, y, size, xSpeed, ySpeed;
+
+        public Ball(int _x, int _y, int _size, int _xSpeed, int _ySpeed)
+        {
+            x = _x;
+            y = _y;
+            size = _size;
+            xSpeed = _xSpeed;
+            ySpeed = _ySpeed;
+        }
+
+        public void Move(int xSpeed, int ySpeed)
+        {
+            x += xSpeed;
+            y += ySpeed;
+        }
+
+        public bool Collision(Ball b)
+        {
+            Rectangle rec1 = new Rectangle(b.x, b.y, b.size, b.size);
+            Rectangle rec2 = new Rectangle(x, y, size, size);
+
+            if (rec1.IntersectsWith(rec2))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ScreenSaver/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+    public partial class Form1 : Form
+    {
+        List<Ball> balls = new List<Ball>();
+
+        SolidBrush drawBrush = new SolidBrush(Color.Red);
+
+        Random rng = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartUp();
+        }
+
+        public void StartUp()
+        {
+            Ball ballOne = new Ball(rng.Next(0, this.Width - 50), rng.Next(0, this.Height - 50), rng.Next(5, 50), rng.Next(-6, 5), rng.Next(-11, 10));
+            Ball ballTwo = new Ball(rng.Next(0, this.Width - 50), rng.Next(0, this.Height - 50), rng.Next(5, 50), rng.Next(-6, 5), rng.Next(-11, 10));
+            Ball ballThree = new Ball(rng.Next(0, this.Width - 50), rng.Next(0, this.Height - 50), rng.Next(5, 50), rng.Next(-6, 5), rng.Next(-11, 10));
+
+            balls.Add(ballOne);
+            balls.Add(ballTwo);
+            balls.Add(ballThree);
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //move each ball
+            foreach (Ball b in balls)
+            {
+                b.Move(b.xSpeed, b.ySpeed);
+            }
+
+            foreach (Ball b in balls)
+            {
+                if (b.x > this.Width - b.size || b.x < 0) { b.xSpeed = -b.xSpeed; }
+                if (b.y > this.Height - b.size || b.y < 0) { b.ySpeed = -b.ySpeed; }
+            }
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Ball b in balls)
+            {
+                e.Graphics.FillEllipse(drawBrush, b.x, b.y, b.size, b.size);
+            }
+        }
+    }
+}
